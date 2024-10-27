6f90760c7fafae6c30269fbca4f541879b1d8437
ivanbelenky <ivandietert@gmail.com>
2024-10-27 00:55:57 -0300

    mathematical details documentation finished

--- lib/word2_vec.ex
+++ lib/word2_vec.ex
@@ -11,10 +11,41 @@ defmodule Word2Vec do
 
   There are two main mathematical models to apply.
   - CBOW: sum of neighbors should be close to the vector of the word
-    N = {-4, -3, -2, -1, +1, +2, +3, +4}
+    N = {-4, -3, -2, -1, +1, +2, +3, +4} is the index set of words that
+    will be considered the neighborhood of word at index 0. The training
+    objective is to maximize the quantity:
+
+    prod_{i in C}( Pr(w_i | w_j: j in N+i) --> stabilize via log
+    sum_{i in C}(ln Pr(w_i | w_j: j in N+i) --> where probability is
+    Pr(w | w_j: j in N + i) := softmax(v*v_w) where
+    v = sum_j(v_{w_j}) and
+    v_w = {v(w): w in V}
+
+    So essentially is calculating a probability for every single
+    word in the vocabulary, there will be a probability. After
+    applying a logarithm, you end up with
+
+    ln Pr(w | wj: j in N+i) = <v_w, v_wj> - ln sum_w( exp(<v_w, v_wj>) )
+
+    therefore we can specify the optimization problem as the maximization
+    of the sum over all the corpus of the above probability. The
+    compounded, and simplectic addition of probabilities results in
+    the desired output.
+
+    // TODO: what about punctuation symbols and such? How does
+    // the corpus look like int this regard
 
   - skip-gram: word vector should be close to the neighbours
+    with the same set N as the CBOW model the idea behind the skip gram model
+    is to maximize the probability of close words given the 0th index vocabulary.
+    This is
 
+    prod_{i in C} = prod( Pr(w_j: j in N+i | w_i) )
+    we transform to a logarithm for numerical stability, but the essence is exactly
+    the same. Maximizing this optimization functino, gives rise to pretty much the same
+    function with a slight difference
+
+    ln Pr(w | wj: j in N+i) = <v_w, v_wj> - ln sum_w( exp(<v_w, v_wi>) )
 
   """
 
